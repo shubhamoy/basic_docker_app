bcbf27d28df653fecaabebbde0f86611daaa1514
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-03-15 20:40:36 +0530

    Basic App

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+let http = require('http');
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+let server = http.Server(app);
+let io = require('socket.io')(server);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+io.sockets.on('connection', (socket) => {
+  setInterval(() => {
+    let recData = getRandomInt(50, 100);
+		let tataData = getRandomInt(75, 150);
+		let gailData = getRandomInt(25, 125);
+		io.sockets.emit('tickerData', [
+			{rdata: recData},
+			{tdata: tataData},
+			{gdata: gailData}
+		]);
+	}, 1000);
+});
+
+server.listen(port, "0.0.0.0");
